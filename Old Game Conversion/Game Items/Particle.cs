de5fcf0630df1d49f1c77fc0abd20215184e04c9
Thomas Micol <thomasmicol@gmail.com>
2019-02-01 01:03:43 +1300

    added a rudimentary particle system which will be used for more gameification of the dropping of gold exp or items

---
+++ Old Game Conversion/Game Items/Particle.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Old_Game_Conversion.Game_Items
+{
+    class Particle
+    {
+        public void Draw(GameTime gameTime, Game1 context)
+        {
+
+        }
+
+        public void Update(GameTime gameTime, Game1 context)
+        {
+
+        }
+    }
+}
---
+++ Old Game Conversion/Game Items/ParticleEmitter.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Old_Game_Conversion.Game_Items
+{
+    class ParticleEmitter
+    {
+        protected List<Particle> particles;
+        protected Particle[] particleSet;
+        protected Vector2 position;
+        protected int particleAmount;
+        protected Vector2 particleVelocityMin;
+        protected Vector2 particleVelocityMax;
+        protected Timer timer;
+        protected int emissionCounter;
+
+        public ParticleEmitter(Vector2 aPosition, int aParticleAmount, Vector2 aParticleVelocityMin, Vector2 aParticleVelocityMax, params Particle[] aParticleSet)
+        {
+            particles = new List<Particle>();
+            particleSet = aParticleSet;
+            position = aPosition;
+            particleAmount = aParticleAmount;
+            particleVelocityMin = aParticleVelocityMin;
+            particleVelocityMax = aParticleVelocityMax;
+            emissionCounter = 0;
+            timer = new Timer(500);
+            timer.AutoReset = true;
+        }
+
+        public void Draw(GameTime gameTime, Game1 context)
+        {
+            foreach(Particle aPart in particles)
+            {
+                aPart.Draw(gameTime, context);
+            }
+        }
+
+        public void Update(GameTime gameTime, Game1 context)
+        {
+            if(emissionCounter > particleAmount)
+            {
+                timer.AutoReset = false;
+            }
+            foreach (Particle aPart in particles)
+            {
+                aPart.Update(gameTime, context);
+            }
+        }
+    }   
+}
