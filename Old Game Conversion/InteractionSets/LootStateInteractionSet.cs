3406cec4bd754ad069248c32a98d183d5b2b70b6
Thomas Micol <thomasmicol@gmail.com>
2019-02-03 15:23:59 +1300

    Added Construction code and the concrete classes for the implementation of the loot screen

--- Old Game Conversion/InteractionSets/LootStateInteractionSet.cs
+++ Old Game Conversion/InteractionSets/LootStateInteractionSet.cs
@@ -3,10 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Old_Game_Conversion.Game_Items;
+using Old_Game_Conversion.Loot_State_Items;
 
 namespace Old_Game_Conversion.InteractionSets
 {
     class LootStateInteractionSet : InteractionSet
     {
+        protected BattleReport report;
+
+        public LootStateInteractionSet()
+        {
+            guiElements = new List<GuiElement>()
+            {
+                new LootScreenFrame(),
+                new LootVictoryLabel(report),
+                new ExpGainedBars(report),
+                new LootGainedBoxes(report),
+                new ContinueButton()
+            };
+
+        }
+
     }
 }
